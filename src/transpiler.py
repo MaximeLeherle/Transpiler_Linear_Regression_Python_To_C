0e5f12f14d917c26194eb0b5a2e5bc6abb38f5d5
MaximeLeherle <69516306+MaximeLeherle@users.noreply.github.com>
2021-12-14 21:26:21 +0100

    Update transpiler.py

--- src/transpiler.py
+++ src/transpiler.py
@@ -1,8 +1,15 @@
+# Import
+
 import os
 import sys
 
 import numpy as np
 import joblib
-import sklearn
-import seabord as sns
+import pandas as pd
 
+import sklearn
+from sklearn import datasets
+from sklearn.metrics import r2_score
+from sklearn.metrics import mean_squared_error
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split

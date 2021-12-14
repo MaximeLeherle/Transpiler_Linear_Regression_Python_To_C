b418d17a10a4dbe92502138432daac362b80dd2f
MaximeLeherle <69516306+MaximeLeherle@users.noreply.github.com>
2021-12-14 21:56:00 +0100

    Create test.sh

---
+++ test.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+python3 -m venv venv
+venv/bin/activate
+
+pip3 install -r requirements.txt
+
+echo -e "Start the python file\n\n\n"
+python3 src/transpiler.py
+
+echo -e "\n\n\nCompile and execute the .c file"
+gcc *.c
+./a.out
